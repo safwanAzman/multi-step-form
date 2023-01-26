e40e6fb7b4615ceb308aa7f10e91c0b14565177f
safwan <safwanazman017@gmail.com>
2023-01-26 10:13:48 +0800

    feat: setup colors tailiwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    // Or if using `src` directory:
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          marineBlue: "#EEF4FB",
+          purplishBlue: "#473DFF",
+          pastelBlue: "#ADBEFF",
+          lightBlue: "#BFE2FD",
+          strawberry: "#ED3548",
+        },
+        neutral: {
+          coolGray: "#9699AB",
+          lightGray: "#D6D9E6",
+          magnolia: "#F0F6FF",
+          alabaster: "#FAFBFF",
+        },
+      },
+    },
   },
   plugins: [],
 }
