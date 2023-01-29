94c18dc907f37d6b592adde607cf1df4b8d92dc9
safwan <safwanazman017@gmail.com>
2023-01-29 08:23:32 +0800

    feat: add sidebar data

---
+++ src/components/sidebar/index.js
@@ -0,0 +1,33 @@
+export const step = [
+    {
+        id : 1,
+        stepNo: '1',
+        title: 'STEP 1',
+        message : 'YOUR INFO',
+        state: ''
+    },
+    {
+        id : 2,
+        stepNo: '2',
+        title: 'STEP 2',
+        message : 'SELECT PLAN',
+        state: ''
+    },
+    {
+        id : 3,
+        stepNo: '3',
+        title: 'STEP 3',
+        message : 'ADD-ONS',
+        state: ''
+    },
+    {
+        id : 4,
+        stepNo: '4',
+        title: 'STEP 4',
+        message : 'SUMMARY',
+        state: ''
+    },
+    
+] 
+
+
