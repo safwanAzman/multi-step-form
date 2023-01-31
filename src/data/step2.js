d3aa0c313d9afe95db24e411496007e890d03289
safwan <safwanazman017@gmail.com>
2023-01-31 10:22:55 +0800

    fix:data step2

--- src/data/step2.js
+++ src/data/step2.js
@@ -5,22 +5,22 @@ export const plans = [
     {
         name: 'Arcade',
         icon: Arcade,
-        priceMo: '$9/mo',
-        priceYr: '$90/yr',
+        priceMo: 9,
+        priceYr: 90,
         tab: '1'
     },
     {
         name: 'Advanced',
         icon: Advanced,
-        priceMo: '$12/mo',
-        priceYr: '$120/yr',
+        priceMo: 12,
+        priceYr: 120,
         tab: '2'
     },
     {
         name: 'Pro',
         icon: Pro,
-        priceMo: '$15/mo',
-        priceYr: '$150/yr',
+        priceMo: 15,
+        priceYr: 150,
         tab: '3'
     }
     
