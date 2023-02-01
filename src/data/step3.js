0baa2c6bd986aeed7367cfdc5a3611ccf62abb60
safwan <safwanazman017@gmail.com>
2023-02-01 14:00:00 +0100

    fix:data step3

--- src/data/step3.js
+++ src/data/step3.js
@@ -3,22 +3,22 @@ export const picks = [
         id:'onlineservice',
         name: 'Online service',
         desc: 'Access to multiplayer games',
-        priceAddMo : '+$1/mo',
-        priceAddYr : '+$10/yr'
+        priceAddMo : 1,
+        priceAddYr : 10
     },
     {
         id:'largerstorage',
         name: 'Larger storage',
         desc: 'Extra 1TB of cloud save',
-        priceAddMo : '+$2/mo',
-        priceAddYr : '+$20/yr'
+        priceAddMo : 2,
+        priceAddYr : 20
     },
     {
         id:'custom',
         name: 'Customizable Profile',
         desc: 'Custom theme on your profile',
-        priceAddMo : '+$1/mo',
-        priceAddYr : '+$10/yr'
+        priceAddMo : 1,
+        priceAddYr : 10
     }
     
 ];
