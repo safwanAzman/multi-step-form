b5a6055265f10521f373a8577f5358112b430d8b
wan <47422904+safwanAzman@users.noreply.github.com>
2024-08-01 21:26:30 +0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -142,7 +142,7 @@ export default function Home() {
       <footer className="fixed top-0 w-full p-2 bg-black ">
           <div className="flex items-center justify-center text-xs text-white">
             Challenge by <a className="px-1 text-teal-500 border-b-2 border-teal-500" href="https://www.frontendmentor.io/">Frontend Mentor.</a>
-            Coded by <a className="pl-1 text-teal-500 border-b-2 border-teal-500" href="https://portfolio-v2-self.vercel.app/"> SafwanAzman.</a>
+            Coded by <a className="pl-1 text-teal-500 border-b-2 border-teal-500" href="https://www.safwan-azman.dev"> SafwanAzman.</a>
           </div>
       </footer>
     </main>
